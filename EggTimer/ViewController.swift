871ac614d1b826cfc6635cd736d754e1cbb1d1e2
ivan_cardenas <ivan.cardenas@infinixsoft.com>
2023-02-22 00:31:34 -0300

    finished app

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -7,39 +7,57 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     
-    var counter = 60
-    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    var timer = Timer()
+    var totalTime = 0
+    var secondsPassed = 0
+    
+    var audioPlayer: AVAudioPlayer?
 
     @IBAction func hardnessSelected(_ sender: UIButton) {
+        
+        resetApp()
+        
         guard let hardness = sender.currentTitle else {return}
         hardnessHandle(hardness: hardness)
         
-        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
     }
     
     private func hardnessHandle(hardness: String){
-        if let hardnessTime = self.eggTimes[hardness] {
-            switch hardness {
-                case "Soft":
-                    print(hardnessTime)
-                case "Medium":
-                    print(hardnessTime)
-                case "Hard":
-                    print(hardnessTime)
-                default:
-                    print("No time for this hardness")
-            }
-        }
+        timer.invalidate()
+        guard let hardnessTime = eggTimes[hardness] else {return}
+        self.totalTime = hardnessTime
     }
     
     @objc func updateCounter() {
-        //example functionality
-        if counter > 0 {
-            print("\(counter) seconds")
-            counter -= 1
+        if secondsPassed < totalTime {
+            secondsPassed += 1
+            self.progressBar.progress = Float(secondsPassed) / Float(totalTime)
+        } else {
+            timer.invalidate()
+            self.titleLabel.text = "Egg Done"
+            playAlert()
         }
+    }
+    
+    private func resetApp() {
+        self.titleLabel.text = "How do you like your eggs?"
+        self.progressBar.progress = 0.0
+        self.secondsPassed = 0
+        self.totalTime = 0
+    }
+    
+    private func playAlert() {
+        let url = Bundle.main.url(forResource: "alarm_sound", withExtension: "mp3")
+        audioPlayer = try! AVAudioPlayer(contentsOf: url!)
+        audioPlayer?.play()
     }
 }
