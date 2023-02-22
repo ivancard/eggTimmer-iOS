a49d1d825edad4909505e856be3739bcbb89e92e
ivan_cardenas <ivan.cardenas@infinixsoft.com>
2023-02-21 23:15:14 -0300

    first commit

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,36 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var counter = 60
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
 
-
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        guard let hardness = sender.currentTitle else {return}
+        hardnessHandle(hardness: hardness)
+        
+        Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+    }
+    
+    private func hardnessHandle(hardness: String){
+        if let hardnessTime = self.eggTimes[hardness] {
+            switch hardness {
+                case "Soft":
+                    print(hardnessTime)
+                case "Medium":
+                    print(hardnessTime)
+                case "Hard":
+                    print(hardnessTime)
+                default:
+                    print("No time for this hardness")
+            }
+        }
+    }
+    
+    @objc func updateCounter() {
+        //example functionality
+        if counter > 0 {
+            print("\(counter) seconds")
+            counter -= 1
+        }
+    }
 }
